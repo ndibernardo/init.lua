16d86fd57e1d253a0c488608b5a53cab87048383
nildr <119125201+nildr@users.noreply.github.com>
2024-07-06 18:33:48 +0200

    Add oil.

--- lua/nicola/lazy/colors.lua
+++ lua/nicola/lazy/colors.lua
@@ -3,10 +3,15 @@ return {
   name = "gruvbox",
   config = function()
     require("gruvbox").setup({
-      terminal_colors = true,
+      terminal_colors = false,
       contrast = "hard",
       palette_overrides = {
-        dark0_hard = "#0C1113",
+        -- dark0_hard = "#0C1113",
+      },
+      overrides = {
+        ["@punctuation.bracket"] = { fg = "#fe8019" },
+        ["@punctuation.delimiter"] = { fg = "#fe8019" },
+        ["@punctuation.special"] = { fg = "#fe8019" },
       },
       italic = {
         strings = false,
