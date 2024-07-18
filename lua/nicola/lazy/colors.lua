4c14d3f7e45581e3a3af318e6c8e91e42f76a55f
nildr <119125201+nildr@users.noreply.github.com>
2024-07-18 22:09:38 +0200

    base16 as colorscheme.

--- lua/nicola/lazy/colors.lua
+++ lua/nicola/lazy/colors.lua
@@ -1,28 +1,8 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  name = "gruvbox",
-  config = function()
-    require("gruvbox").setup({
-      terminal_colors = false,
-      contrast = "hard",
-      palette_overrides = {
-        -- dark0_hard = "#0C1113",
-      },
-      overrides = {
-        ["@punctuation.bracket"] = { fg = "#fe8019" },
-        ["@punctuation.delimiter"] = { fg = "#fe8019" },
-        ["@punctuation.special"] = { fg = "#fe8019" },
-      },
-      italic = {
-        strings = false,
-        emphasis = false,
-        comments = false,
-        operators = false,
-        folds = false,
-      },
-      bold = false,
-    })
-    vim.o.background = "dark"
-    vim.cmd("colorscheme gruvbox");
-  end,
+  {
+    "chriskempson/base16-vim",
+    config = function()
+      vim.cmd("colorscheme base16-default-dark")
+    end
+  }
 }
