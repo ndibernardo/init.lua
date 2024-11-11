eac57ed7a8064097d487e5e01bf6c72dfc4f0611
Nicola Di Bernardo <nicola@macbook.homenet.telecomitalia.it>
2024-11-11 14:20:50 +0100

    fix ts lsp server

--- lua/nicola/lazy/lsp.lua
+++ lua/nicola/lazy/lsp.lua
@@ -9,7 +9,7 @@ return {
   },
   config = function()
     vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      group = vim.api.nvim_create_augroup("lsp-attach", { clear = true }),
       callback = function(event)
         local map = function(keys, func, desc)
           vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
@@ -53,7 +53,7 @@ return {
     local servers = {
       clangd = {},
       rust_analyzer = {},
-      tsserver = {},
+      ts_ls = {},
       zls = {},
       lua_ls = {
         settings = {
