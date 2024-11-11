96fb4458079ee7b52c148b65703a3cdc4ea5e7f6
Nicola Di Bernardo <nicola@macbook.homenet.telecomitalia.it>
2024-11-11 14:20:13 +0100

    add modus themes

--- lua/nicola/lazy/colors.lua
+++ lua/nicola/lazy/colors.lua
@@ -2,7 +2,14 @@ return {
   {
     'aktersnurra/no-clown-fiesta.nvim',
     config = function()
-      vim.cmd [[colorscheme no-clown-fiesta]]
+      -- vim.cmd [[colorscheme no-clown-fiesta]]
     end
   },
+  {
+    "miikanissi/modus-themes.nvim",
+    priority = 1000,
+    config = function()
+      vim.cmd [[colorscheme modus_operandi]]
+    end
+  }
 }
